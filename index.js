0f864b67fec23eb0f6c260265e72048e1467f93c
Qudix <17361645+Qudix@users.noreply.github.com>
2018-06-29 13:01:56 -0500

    1.1.0
    Updated for zEdit 0.4.2

--- index.js
+++ index.js
@@ -6,24 +6,21 @@ registerPatcher({
         label: 'Opposite Animation Disabler',
         hide: true
     },
-    requiredFiles: [],
     getFilesToPatch: function(filenames) {
         return filenames;
     },
-    execute: {
+    execute: (patchFile, helpers, settings, locals) => ({
         process: [{
-            load: function(plugin, helpers, settings, locals) {
-                return {
-                    signature: 'NPC_',
-                    filter: function(record) {
-                        return xelib.GetFlag(record, 'ACBS\\Flags', 'Opposite Gender Anims');
-                    }
+            load: {
+                signature: 'NPC_',
+                filter: function(record) {
+                    return xelib.GetFlag(record, 'ACBS\\Flags', 'Opposite Gender Anims');
                 }
             },
-            patch: function(record, helpers, settings, locals) {
-                helpers.logMessage("Disabling 'Opposite Gender Anims' for " + xelib.LongName(record));
+            patch: function(record) {
+                helpers.logMessage(`Disabling 'Opposite Gender Anims' for ${xelib.LongName(record)}`);
                 xelib.SetFlag(record, 'ACBS\\Flags', 'Opposite Gender Anims', false); 
             }
         }]
-    }
+    })
 });
